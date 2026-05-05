06e0edee395988cafa9c0c1089f1e798886f30e3
GustavoGmBr <gustavonovaisgm@gmail.com>
2026-05-05 00:59:50 -0300

    deploy: 05/05/2026, 00:59:50

--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,57 @@
 import { Router } from 'express';
-import personagemPublic from './Public/personagem.routes.js';
-import personagemPrivate from './Private/personagem.routes.js';
-import authRoutes from './Public/auth.routes.js';
-import racaPublic from './Public/raca.routes.js';
-import racaPrivate from './Private/raca.routes.js';
-import sistemaPublic from './Public/sistema.routes.js';
-import sistemaPrivate from './Private/sistema.routes.js';
-import historicoPublic from './Public/historico.routes.js';
-import historicoPrivate from './Private/historico.routes.js';
-import livroPublic from './Public/livro.routes.js';
-import livroPrivate from './Private/livro.routes.js';
-import capituloPublic from './Public/capitulo.routes.js';
-import capituloPrivate from './Private/capitulo.routes.js';
-import itemPublic from './Public/item.routes.js';
-import itemPrivate from './Private/item.routes.js';
-import localPublic from './Public/locais.routes.js';
-import localPrivate from './Private/locais.routes.js';
+import personagempublic from './public/personagem.routes.js';
+import personagemprivate from './private/personagem.routes.js';
+import authRoutes from './public/auth.routes.js';
+import racapublic from './public/raca.routes.js';
+import racaprivate from './private/raca.routes.js';
+import sistemapublic from './public/sistema.routes.js';
+import sistemaprivate from './private/sistema.routes.js';
+import historicopublic from './public/historico.routes.js';
+import historicoprivate from './private/historico.routes.js';
+import livropublic from './public/livro.routes.js';
+import livroprivate from './private/livro.routes.js';
+import capitulopublic from './public/capitulo.routes.js';
+import capituloprivate from './private/capitulo.routes.js';
+import itempublic from './public/item.routes.js';
+import itemprivate from './private/item.routes.js';
+import localpublic from './public/locais.routes.js';
+import localprivate from './private/locais.routes.js';
 const router = Router();
 
 // 🔑 Autenticação
 router.use('/auth', authRoutes);
 
 // 👥 Personagens
-router.use('/public/personagens', personagemPublic);
-router.use('/private/personagens', personagemPrivate);
+router.use('/public/personagens', personagempublic);
+router.use('/private/personagens', personagemprivate);
 
 // 🧌 Racas
-router.use('/public/racas', racaPublic);
-router.use('/private/racas', racaPrivate);
+router.use('/public/racas', racapublic);
+router.use('/private/racas', racaprivate);
 
 // 🖥️ Sistema
-router.use('/public/sistemas', sistemaPublic);
-router.use('/private/sistemas', sistemaPrivate);
+router.use('/public/sistemas', sistemapublic);
+router.use('/private/sistemas', sistemaprivate);
 
 // 📜 Histórico
-router.use('/public/historicos', historicoPublic);
-router.use('/private/historicos', historicoPrivate);
+router.use('/public/historicos', historicopublic);
+router.use('/private/historicos', historicoprivate);
 
 // 📖 Livro
-router.use('/public/livros', livroPublic);
-router.use('/private/livros', livroPrivate);
+router.use('/public/livros', livropublic);
+router.use('/private/livros', livroprivate);
 
 
 // 📄 Capitulo
-router.use('/public/capitulos', capituloPublic);
-router.use('/private/capitulos', capituloPrivate);
+router.use('/public/capitulos', capitulopublic);
+router.use('/private/capitulos', capituloprivate);
 
 // ⚔️ Itens
-router.use('/public/itens', itemPublic);
-router.use('/private/itens', itemPrivate);
+router.use('/public/itens', itempublic);
+router.use('/private/itens', itemprivate);
 
 // 📍 Locais
-router.use('/public/locais', localPublic);
-router.use('/private/locais', localPrivate);
+router.use('/public/locais', localpublic);
+router.use('/private/locais', localprivate);
 
 export default router;
