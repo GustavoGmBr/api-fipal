a2941f4100f53d89372aaee69166cf1ee233d248
GustavoGmBr <gustavonovaisgm@gmail.com>
2026-05-16 18:34:29 -0300

    Fix: resolvendo conflitos de merge 2

--- routes/private/inventario.routes.js
+++ routes/private/inventario.routes.js
@@ -10,10 +10,5 @@ router.use(authMiddleware);
 router.post('/', inventarioController.store);
 router.put('/:id', inventarioController.update);
 router.delete('/:id', inventarioController.destroy);
-<<<<<<< HEAD
 router.patch('/:id/dinheiro', inventarioController.updateDinheiro); // Corrigido
-=======
-router.patch('/inventario/:id/dinheiro', inventarioController.updateDinheiro);
-
->>>>>>> ca0e9fd5ddc13ee00a8f065b118678c503356742
 export default router;
